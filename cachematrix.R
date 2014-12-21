af9c5e217406b20b08790b19147aecf1782f482d
flbufl <flbufl@gmail.com>
2014-12-21 16:24:34 -0500

    Improve readability

--- cachematrix.R
+++ cachematrix.R
@@ -14,9 +14,9 @@ makeCacheMatrix <- function(x = matrix()) {
         x <<- y
         inv <<- NULL
     }
-    get <- function() x
-    setinv <- function(solve) inv <<- solve
-    getinv <- function() inv
+    get <- function() { x }
+    setinv <- function(solve) { inv <<- solve }
+    getinv <- function() { inv }
     list(set = set, get = get,
          setinv = setinv,
          getinv = getinv)
